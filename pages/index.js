f42ade380253c05a91caa28492bbc638fc9a22d1
mangelito74 <mangelito74@gmail.com>
2023-09-14 14:23:02 +0200

    Added viewport meta tag

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const HomePage = (props) => {
       <Head>
         <title>SnareMaster</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <ProductList products={props.products} />
